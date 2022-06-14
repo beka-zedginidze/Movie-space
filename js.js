a821f861f86cc99d52fe56ed946b2f3198a188ec
beka <beka.zedginidze.1@btu.edu.ge>
2022-06-14 23:08:40 +0400

    add by beka

---
+++ js.js
@@ -0,0 +1,10 @@
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    initialSlide : 1,
+    centeredSlides: true,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    }
+  });
